b6280ce370cb168985f636bfa625fc68a8db9b2e
CristianOz <mayteoelan@gmail.com>
2021-11-02 18:11:14 -0500

    login y register

--- app.js
+++ app.js
@@ -9,4 +9,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
 });
 
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/register.html'));
+});
+
 app.listen(port, ()=>console.log(`Servidor corriendo en el puerto ${port}`));

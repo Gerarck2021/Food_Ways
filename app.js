f8ac58a6026b833abb3facd55780731d284b7520
CristianOz <mayteoelan@gmail.com>
2021-11-02 21:46:37 -0500

    avanzando con la pagina

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 5001;
+const port = process.env.PORT || 5000;
+
 
 app.use(express.static(path.resolve(__dirname, './public')));
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/index.html'));
@@ -23,4 +25,3 @@ app.get('/carrito', (req, res) => {
 app.get('/detalle-reservacion', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/detalleReservacion.html'));
 }); 
-app.listen(port, ()=>console.log(`Servidor corriendo en el puerto ${port}`));
